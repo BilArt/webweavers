dfe7b4cdbcf383fafcf119a9007419b77da67adb
Artem Bilousov <artem.bilousov.1993@gmail.com>
2024-05-11 21:47:53 +0200

    hero btn
    Corrected hero btn

--- src/js/proposal-animation.js
+++ src/js/proposal-animation.js
@@ -5,15 +5,15 @@ const bookImg = container.querySelector('.proposal-books-img');
 
 const windowHeight = window.innerHeight;
 
-window.addEventListener('scroll', function () {    
+window.addEventListener('scroll', function () {
     const containerTop = container.getBoundingClientRect().top;
     const containerBottom = container.getBoundingClientRect().bottom;
-    
-    if (containerTop > 0 && containerBottom < windowHeight) {        
+
+    if (containerTop > 0 && containerBottom < windowHeight) {
         rotate11.classList.add('rotate-11');
         rotate9.classList.add('rotate-9');
         bookImg.classList.add('books-bounce');
-    } else {       
+    } else {
         rotate11.classList.remove('rotate-11');
         rotate9.classList.remove('rotate-9');
         bookImg.classList.remove('books-bounce');
