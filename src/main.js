8f644f049354b5d352ef3732a0f517e5feeefb03
Artem Bilousov <artem.bilousov.1993@gmail.com>
2024-05-12 20:25:27 +0200

    Animation
    Added scroll animation

--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
-import "./js/menu.js"
-import "./js/proposal-animation.js"
+import './js/menu.js';
+import './js/proposal-animation.js';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init();
